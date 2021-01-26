ba09d06f1bbcc486cbc8cb0c8c81e24586e41179
Aleksei Alekseikov <62751381+AlekseiAlekseikov@users.noreply.github.com>
2021-01-26 17:24:37 +0300

    Add files via upload

---
+++ config/db_params.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'host' => 'localhost', // адрес сервера
+    'dbname' => 'phpshop', // имя базы данных
+    'user' => 'root', // имя пользователя
+    'password' => '', // пароль
+             );
+?>
